9866e3a4cd17986348ee077496e07923ccba8e11
Ayushi12578 <ayushigupta4279@gmail.com>
2026-08-13 22:47:05 +0530

    Initial commit

---
+++ EmployeeBadge.java
@@ -0,0 +1,16 @@
+package Varibale;
+
+public class EmployeeBadge {
+    public static void main(String[] args) {
+        int id=00042;
+        String name="priya Sharma";
+        String department="Engg";
+        Boolean ismanager=true;
+        System.out.println(id);
+        System.out.println(name);
+        System.out.println(department);
+        System.out.println(ismanager);
+
+
+    }
+}
