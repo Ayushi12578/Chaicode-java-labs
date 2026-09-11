5313a0960b3596679e0fdd071833ddd8b21510bb
Ayushi12578 <ayushigupta4279@gmail.com>
2026-09-11 12:33:47 +0530

    Add practice program

---
+++ Looping/WhileLoop/practice.java
@@ -0,0 +1,16 @@
+package Looping.WhileLoop;
+
+public class practice {
+    public static void main(String[] args) {
+        int i =100;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(i<=102);
+            System.out.println(i);
+            i++;
+        
+       
+    }
+}
